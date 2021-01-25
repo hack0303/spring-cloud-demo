356f938cf965bfb53d6af643554f47f38076db69
hack0303 <1097037585@qq.com>
2021-01-26 02:41:19 +0800

    1.版本统一

--- spring-cloud-simple-client-demo/sping-cloud-simple-eureka-client-demo/src/main/java/org/cloud/www/EurekaSimpleClient.java
+++ spring-cloud-simple-client-demo/sping-cloud-simple-eureka-client-demo/src/main/java/org/cloud/www/EurekaSimpleClient.java
@@ -2,14 +2,12 @@ package org.cloud.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
-public class EurekaServer {
+public class EurekaSimpleClient {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServer.class, args);
+        SpringApplication.run(EurekaSimpleClient.class, args);
     }
 
 }
---
+++ spring-cloud-simple-client-demo/sping-cloud-simple-eureka-client-demo/src/main/java/org/cloud/www/controllers/ServiceInstanceRestController.java
@@ -0,0 +1,7 @@
+package org.cloud.www.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceInstanceRestController {
+}
