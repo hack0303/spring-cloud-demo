1ca5109d345ea1c3e293988fe6df2e4a5c7a9e2b
hack0303 <1097037585@qq.com>
2021-01-26 03:13:09 +0800

    1.eureka server and client

--- spring-cloud-simple-client-demo/sping-cloud-simple-eureka-client-demo/src/main/java/org/cloud/www/controllers/ServiceInstanceRestController.java
+++ spring-cloud-simple-client-demo/sping-cloud-simple-eureka-client-demo/src/main/java/org/cloud/www/controllers/ServiceInstanceRestController.java
@@ -1,7 +1,24 @@
 package org.cloud.www.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ServiceInstanceRestController {
+
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
 }
