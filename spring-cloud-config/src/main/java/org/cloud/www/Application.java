cf2c5dcdf3c801d54f2865df6b0634103d3e95e5
hack0303 <1097037585@qq.com>
2021-02-07 17:45:09 +0800

    1.配置中心

--- spring-cloud-config/src/main/java/org/cloud/www/Application.java
+++ spring-cloud-config/src/main/java/org/cloud/www/Application.java
@@ -3,24 +3,22 @@ package org.cloud.www;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Configuration
-@EnableAutoConfiguration
-@RestController
+/**
+ *
+ * https://docs.spring.io/spring-cloud-config/docs/current/reference/html/#_nested_keys_in_vault
+ *
+ * ref:https://blog.csdn.net/wo18237095579/article/details/83623262?utm_medium=distribute.pc_relevant.none-task-blog-searchFromBaidu-2.control&depth_1-utm_source=distribute.pc_relevant.none-task-blog-searchFromBaidu-2.control
+ * */
+@SpringBootApplication
 @EnableConfigServer
 public class Application {
-
-    @Value("${config.name}")
-    String name = "World";
-
-    @RequestMapping("/")
-    public String home() {
-        return "Hello " + name;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
